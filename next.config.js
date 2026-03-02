1c260c4900d5ff53659083fac5c0f126fcd5207c
Yashomati <yashomati203@gmail.com>
2026-03-02 17:16:04 +0530

    chore: force vercel region to iad1 and enable standalone output

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
     reactStrictMode: true,
     images: {
         domains: [
